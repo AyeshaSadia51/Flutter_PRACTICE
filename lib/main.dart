7bfe9551296ad1ec01e38589bc1f49050e81b42d
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-09-10 22:14:33 +0600

    Buttons, TextField, Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,152 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp (MyApp());
+void main(){
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(                  /// MaterialApp - set all config of an app
-
-      debugShowCheckedModeBanner: true,
-
+    return MaterialApp(
       home: HomeScreen(),
-      title: "OSTAD APP",
-
+      title: 'Practice App',
     );
   }
 }
 
 class HomeScreen extends StatelessWidget{
-
   @override
   Widget build(BuildContext context) {
-   return Scaffold(                  /// Scaffold (homescreen) 2 part - appBar & body
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Profile',style: TextStyle(fontSize: 25),),
+        leading: Icon(Icons.person,size: 32,),
+        actions: [Icon(Icons.add_ic_call,size: 32,)],
+      ),
 
-    backgroundColor: Colors.purple,
+      body: Column(
 
-    appBar: AppBar(
-      backgroundColor: Colors.pinkAccent,
-      centerTitle: true,
-      title: Text("HOME"),
-      leading: Icon(Icons.store, size: 36, color: Colors.yellow,),
-      actions: [
-        Icon(Icons.search,size: 36, color: Colors.yellow)
-      ],
 
-    ),
-
-   /* body: Center(
-    child: Text("Home Screen", style: TextStyle(        // SINGLE WIDGET
-      color:  Colors.white,
-      fontSize: 30,
-      fontWeight: FontWeight.bold
-    ),),
-    ),
-*/
-
-     body: Column(
-       children: [
-         Column(                                  // MULTIPLE WIDGET - Column / Row
-           mainAxisAlignment: MainAxisAlignment.center,
-           crossAxisAlignment: CrossAxisAlignment.end,
-
-           children: [
-            Text('Hello!!'),
-            Text('Ayesha', style: TextStyle(
-                color: Colors.amber,fontWeight: FontWeight.bold
-            ),),
-
-            Text('Ferdous', style: TextStyle(
-                color: Colors.amber,fontWeight: FontWeight.bold
-            ),),
-
-             Text('Sadia', style: TextStyle(
-                color: Colors.amber,fontWeight: FontWeight.bold
-            ),),
-
-             Row(
-         mainAxisAlignment: MainAxisAlignment.center,
-         crossAxisAlignment: CrossAxisAlignment.center,
-
-         children: [
-    Text('Hello!! '),
-    Text('Ayesha ', style: TextStyle(
-      color: Colors.amber,fontWeight: FontWeight.bold
-    ),),
-
-     Text('Ferdous ', style: TextStyle(
-        color: Colors.amber,fontWeight: FontWeight.bold
-    ),),
-
-    Text('Sadia ', style: TextStyle(
-    color: Colors.amber,fontWeight: FontWeight.bold
-    ),),
-            ]
-             )
-           ],
-         ),
-         Row(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-             Text('Student of '),
-             Text('Leading '),
-             Text('University ')
-           ],
-         ),
-         Column(
-           children: [
-             Text('Department: ', style: TextStyle(
-             color: Colors.white, fontWeight: FontWeight.bold
-         ),),
-             Text('CSE', style: TextStyle(
-                 color: Colors.limeAccent, fontWeight: FontWeight.bold
-             ),),
-           ],
-         ),
-
-         Image.asset('images/BabyPic.jpeg',
-           width: 150,height: 165, fit: BoxFit.cover,),
-         Image.network('https://c4.wallpaperflare.com/wallpaper/101/380/61/cat-animals-bokeh-cute-wallpaper-preview.jpg',
-                        width: 350, height: 200, fit:BoxFit.cover,
-          ),
-
-         RichText(text: TextSpan(
-           text: 'My ',
-           style: TextStyle(
-             color: Colors.red,
-             fontSize: 30
-           ),
-
-         children: [
-           TextSpan(
-             text: 'phone ',
-             style: TextStyle(
-              color: Colors.blueAccent,
-               fontSize: 18
-             )
-           ),
-           TextSpan(
-             text: 'name',
-             style: TextStyle(
-               color: Colors.pinkAccent,
-               fontWeight: FontWeight.bold,
-               fontSize: 22
-             )
-           ),
-           TextSpan(
-             text: 'Your Phone name',
-             style: TextStyle(
-               color: Colors.amber,
-               //fontSize: 30
-             )
-           ),
-         ]
-         ))
-       ],
-     ),
-   );
+      ),
+    );
   }
+
 }
