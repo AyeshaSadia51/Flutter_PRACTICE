ff4010a72f51ce8540b3c87cfaed23c4c8dbc943
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-09-12 17:12:45 +0600

    Buttons, TextField, Container

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,52 @@ class HomeScreen extends StatelessWidget{
         actions: [Icon(Icons.add_ic_call,size: 32,)],
       ),
 
-      body: Column(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onLongPress: () { print('Tap & Hold the ElevatedButton to Send Money');} ,
+                onPressed: (){ print('Tapped Send Money');},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pinkAccent,
+                  textStyle: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                  )
+                ),
+                child: Text('Send Money')),
+            TextButton(
+                onLongPress: () { print('Hold the TextButton to Send Money');} ,
+                onPressed: (){ print('Tapped Send Money');},
+                style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.pinkAccent,
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )
+                ),
+                child: Text('Resend Money')
+            ),
+            IconButton(onPressed: (){}, icon: Icon(Icons.add_circle_outline,
+              size: 40,
+              color: Colors.purple,)),
+            OutlinedButton(
+                onPressed: (){},
+                onLongPress: (){},
+                style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.pinkAccent,
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    )
+                ),
+                child: Text('Outline Button')),
 
+            TextField(),
 
+          ],
+        ),
       ),
     );
   }
