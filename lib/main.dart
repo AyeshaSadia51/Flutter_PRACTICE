6a28ddef63bea268b94ae25d7f0aca256f030af8
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-09-12 22:11:26 +0600

    Buttons, TextField, Container

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,26 @@ class HomeScreen extends StatelessWidget{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Container(
+              width: 250,
+              height: 200,
+              margin: EdgeInsets.all(16),
+              padding: EdgeInsets.all(10),
+              child: Text('Ayesha',style: TextStyle(color: Colors.amberAccent,fontSize: 25,fontWeight: FontWeight.bold),),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.purple,
+                border: Border.all(color: Colors.black,width: 3),
+               // borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(60),
+                  bottomLeft: Radius.circular(60)
+                ),
+               // shape: BoxShape.circle,
+
+              ),
+            ),
+
             ElevatedButton(
                 onLongPress: () { print('Tap & Hold the ElevatedButton to Send Money');} ,
                 onPressed: (){ print('Tapped Send Money');},
@@ -38,6 +58,9 @@ class HomeScreen extends StatelessWidget{
                   textStyle: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.w500,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18)
                   )
                 ),
                 child: Text('Send Money')),
@@ -69,16 +92,19 @@ class HomeScreen extends StatelessWidget{
                 child: Text('Outline Button')),
 
 
-            TextField(
-              style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),
-              decoration: InputDecoration(
-                fillColor: Colors.pinkAccent,
-                filled: true,
-                hintText: 'Enter your @userID',
-                hintStyle: TextStyle(color: Colors.white70),
-                label: Text('UserID'),
-                labelStyle: TextStyle(color: Colors.white,fontSize: 20),
-                suffixIcon: Icon(Icons.person,color: Colors.white,),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),
+                decoration: InputDecoration(
+                  fillColor: Colors.pinkAccent,
+                  filled: true,
+                  hintText: 'Enter your @userID',
+                  hintStyle: TextStyle(color: Colors.white70),
+                  label: Text('UserID'),
+                  labelStyle: TextStyle(color: Colors.white,fontSize: 20),
+                  suffixIcon: Icon(Icons.person,color: Colors.white,),
+                ),
               ),
             ),
 
@@ -86,27 +112,30 @@ class HomeScreen extends StatelessWidget{
               height: 10,
             ),
 
-            TextField(
-              obscureText: true,
-              style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),
-              decoration: InputDecoration(
-                fillColor: Colors.pinkAccent,
-                filled: true,
-                hintText: 'Enter your password',
-                hintStyle: TextStyle(color: Colors.white70),
-                label: Text('Password'),
-                labelStyle: TextStyle(color: Colors.white,fontSize: 20),
-                suffixIcon: Icon(Icons.password,color: Colors.white,),
-                border: OutlineInputBorder(),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                obscureText: true,
+                style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),
+                decoration: InputDecoration(
+                  fillColor: Colors.pinkAccent,
+                  filled: true,
+                  hintText: 'Enter your password',
+                  hintStyle: TextStyle(color: Colors.white70),
+                  label: Text('Password'),
+                  labelStyle: TextStyle(color: Colors.white,fontSize: 20),
+                  suffixIcon: Icon(Icons.password,color: Colors.white,),
+                  border: OutlineInputBorder(),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red)
+              ),
+                  focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green)
+                    )
                 ),
-                disabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.red)
-            ),
-                focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green)
-                  )
               ),
             ),
 
