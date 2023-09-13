9552c2d8b9407994dee11b9166ecb0f4c63d9ab3
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-09-13 21:32:33 +0600

    GridView

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,10 @@ class HomeScreen extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
-        title: Text('facebook',style: TextStyle(
+        title: Text('Circus',style: TextStyle(
             fontSize: 26
         ),),
-        actions: [Icon(Icons.facebook,size: 25,)],
+        actions: [Icon(Icons.child_care,size: 25,)],
       ),
 
        /*body: Center(
@@ -65,123 +65,70 @@ class HomeScreen extends StatelessWidget{
       body: Scrollbar(
         thickness: 15,
         radius: Radius.circular(20),
-          child: ListView(
-            scrollDirection: Axis.vertical,
+          child: GridView(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2
+            ),
             children: [
-              Icon(
-                  Icons.facebook, size: 80, color: Colors.blueAccent,
-                ),
-              ListTile(
-                title: Text('Mobile number', style: TextStyle(
-                  fontSize: 18, fontWeight: FontWeight.bold
+              Image.network('https://as2.ftcdn.net/v2/jpg/02/66/31/75/1000_F_266317554_kr7DPOoM5Uty0YCeFU9nDZTt4a2LeMJF.jpg'),
+              Center(
+                child: Text('Lets Rock', style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.brown, fontSize: 28
                 ),),
-                subtitle: Text('017********'),
-                leading: Icon(Icons.mobile_friendly),
-                trailing: Icon(Icons.arrow_forward_ios_sharp),
-                onTap: (){print('Moblile No Pressed');},
               ),
-              Divider(
-                thickness: 3,
-                height: 16,
-                color: Colors.blueGrey,
-                indent: 18,
-                endIndent: 18,
-              ),
-              ListTile(
-                title: Text('Email address', style: TextStyle(
-                    fontSize: 18, fontWeight: FontWeight.bold
+              Image.network('https://as2.ftcdn.net/v2/jpg/00/74/15/95/1000_F_74159556_67n5823V7Ei87a4g6JJnYHC0yMSo1AEy.jpg'),
+              Center(
+                child: Text('Lets Chill', style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.orange, fontSize: 28
                 ),),
-                subtitle: Text('username@gmail.com'),
-                leading: Icon(Icons.email),
-                trailing: Icon(Icons.arrow_forward_ios_sharp),
-                onTap: (){print('Email Pressed');},
               ),
-              Divider(
-                thickness: 3,
-                height: 16,
-                color: Colors.blueGrey,
-                indent: 18,
-                endIndent: 18,
-              ),
-              ListTile(
-                title: Text('Password', style: TextStyle(
-                    fontSize: 18, fontWeight: FontWeight.bold
+              Image.network('https://as2.ftcdn.net/v2/jpg/02/66/31/75/1000_F_266317554_kr7DPOoM5Uty0YCeFU9nDZTt4a2LeMJF.jpg'),
+              Center(
+                child: Text('Lets Rock', style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.brown, fontSize: 28
                 ),),
-                subtitle: Text('*************'),
-                leading: Icon(Icons.password),
-                trailing: Icon(Icons.arrow_forward_ios_sharp),
-                onTap: (){print('Password Pressed');},
               ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ElevatedButton(
-                    onPressed: (){ print('Tapped Log In');},
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueAccent,
-                        textStyle: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                        ),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)
-                        )
-                    ),
-                    child: Text('Log In')),
+              Image.network('https://as1.ftcdn.net/v2/jpg/00/28/08/40/1000_F_28084010_bGRJetPfBwNcO3YuRC2C3Pz7qASocWQ4.jpg'),
+              Center(
+                child: Text('Say Cheeeeeezzzz', style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black, fontSize: 28
+                ),),
               ),
-
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-
+              Image.network('https://as2.ftcdn.net/v2/jpg/02/66/31/75/1000_F_266317554_kr7DPOoM5Uty0YCeFU9nDZTt4a2LeMJF.jpg'),
+              Center(
+                child: Text('Lets Rock', style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.brown, fontSize: 28
+                ),),
+              ),
+              Image.network('https://as2.ftcdn.net/v2/jpg/00/74/15/95/1000_F_74159556_67n5823V7Ei87a4g6JJnYHC0yMSo1AEy.jpg'),
+              Center(
+                child: Text('Lets Chill', style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.orange, fontSize: 28
+                ),),
+              ),
+              Image.network('https://as2.ftcdn.net/v2/jpg/02/66/31/75/1000_F_266317554_kr7DPOoM5Uty0YCeFU9nDZTt4a2LeMJF.jpg'),
+              Center(
+                child: Text('Lets Rock', style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.brown, fontSize: 28
+                ),),
+              ),
+              Image.network('https://as1.ftcdn.net/v2/jpg/00/28/08/40/1000_F_28084010_bGRJetPfBwNcO3YuRC2C3Pz7qASocWQ4.jpg'),
+              Center(
+                child: Text('Say Cheeeeeezzzz', style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black, fontSize: 28
+                ),),
+              ),
 
             ],
-          ),
-
-
-
+          )
       ),
 
     );
