5d2c4870a32251d1e0cf165f48b01a98a6520b5b
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-09-13 19:47:06 +0600

    GestureDetector, InkWell, ListView, Scrollbar, ListTile, GridView

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,11 @@ class HomeScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Contact List',style: TextStyle(
-            fontSize: 25
+        backgroundColor: Colors.blueAccent,
+        title: Text('facebook',style: TextStyle(
+            fontSize: 26
         ),),
-        actions: [Icon(Icons.contact_page,size: 25,)],
+        actions: [Icon(Icons.facebook,size: 25,)],
       ),
 
        /*body: Center(
@@ -67,6 +68,68 @@ class HomeScreen extends StatelessWidget{
           child: ListView(
             scrollDirection: Axis.vertical,
             children: [
+              Icon(
+                  Icons.facebook, size: 80, color: Colors.blueAccent,
+                ),
+              ListTile(
+                title: Text('Mobile number', style: TextStyle(
+                  fontSize: 18, fontWeight: FontWeight.bold
+                ),),
+                subtitle: Text('017********'),
+                leading: Icon(Icons.mobile_friendly),
+                trailing: Icon(Icons.arrow_forward_ios_sharp),
+                onTap: (){print('Moblile No Pressed');},
+              ),
+              Divider(
+                thickness: 4,
+                height: 16,
+                color: Colors.blueGrey,
+                indent: 18,
+                endIndent: 18,
+              ),
+              ListTile(
+                title: Text('Email address', style: TextStyle(
+                    fontSize: 18, fontWeight: FontWeight.bold
+                ),),
+                subtitle: Text('username@gmail.com'),
+                leading: Icon(Icons.email),
+                trailing: Icon(Icons.arrow_forward_ios_sharp),
+                onTap: (){print('Email Pressed');},
+              ),
+              Divider(
+                thickness: 4,
+                height: 16,
+                color: Colors.blueGrey,
+                indent: 18,
+                endIndent: 18,
+              ),
+              ListTile(
+                title: Text('Password', style: TextStyle(
+                    fontSize: 18, fontWeight: FontWeight.bold
+                ),),
+                subtitle: Text('*************'),
+                leading: Icon(Icons.password),
+                trailing: Icon(Icons.arrow_forward_ios_sharp),
+                onTap: (){print('Password Pressed');},
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                    onPressed: (){ print('Tapped Log In');},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        textStyle: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)
+                        )
+                    ),
+                    child: Text('Log In')),
+              ),
+
+
               Text('Ayesha',
                 style: TextStyle(fontSize: 22, color: Colors.pink),),
               Text('Ayesha',
@@ -113,36 +176,7 @@ class HomeScreen extends StatelessWidget{
                 style: TextStyle(fontSize: 22, color: Colors.pink),),
               Text('Ayesha',
                 style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
-              Text('Ayesha',
-                style: TextStyle(fontSize: 22, color: Colors.pink),),
+
 
             ],
           ),
