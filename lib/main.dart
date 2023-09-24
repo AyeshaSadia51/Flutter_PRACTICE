3025d1a50a3c375467fc0138008fdb11edc513d7
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-09-24 21:57:30 +0600

    Cupertino- switch, button, TextField, TabBar,

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,30 @@ class Home extends StatelessWidget{
         leading: Icon(CupertinoIcons.home),
         middle: Text('Home'),
         trailing: CupertinoSwitch(
-          onChanged: (bool value) {
+          onChanged: (bool onChange) {
 
-          },value: true,
+          }, value: true,
 
         ),
       ),
 
       child: Center(
-        child: Text('Hello Jawad Bolod'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello Jawad Bolod'),
+            Text('Hello Jawad Bolod'),
+            Text('Hello Jawad Bolod'),
+            Text('Hello Jawad Bolod'),
+            CupertinoButton(child: Text('click'), onPressed: (){}),
+            CupertinoTextField(),
+            CupertinoTabBar(items: [
+              BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(CupertinoIcons.phone, color: CupertinoColors.activeBlue,), label: 'Phone'),
+              BottomNavigationBarItem(icon: Icon(CupertinoIcons.list_bullet, color: CupertinoColors.activeBlue), label: 'Contact'),
+            ])
+          ],
+        ),
       ),
     );
   }}
