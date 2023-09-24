1f1bea8009eaa963f96d0ab41c639fa0b3d062f3
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-09-24 22:52:50 +0600

    Switch, Dialogue, ModalBottomSheet,

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,41 @@ class Home extends StatelessWidget{
              },
              child: Text('Submit')
          ),
+         ElevatedButton(
+             onPressed: (){
+               showAboutDialog(context: context,
+               applicationName: "PickaCHuu",
+                 applicationVersion: '1.0.2',
+                 children: [
+                   Text('Application only for children under 15 age !')
+                 ]
+               );
+             }, child: Text('About')),
+         ElevatedButton(
+             onPressed: (){
+               showModalBottomSheet(
+                 isDismissible: false,
+                 barrierColor: Colors.blue,
+                 shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.only(
+                     topLeft: Radius.circular(40),
+                     topRight: Radius.circular(40),
+                   )
+                 ),
+                   context: context, builder: (context){
+                 return Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: Column(
+                     children: [
+                       Center(
+                         child: Text('This is Bottom Sheet'),
+                       )
+                     ],
+                   ),
+                 );
+               });
+             }, child: Text('Bottom Sheet')),
+
        ],
      ),
    );
