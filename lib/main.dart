0e427aac80a9fb590cacdcce224c4d9799636e5f
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-09-24 21:20:57 +0600

    Cupertino

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+
       navigationBar: CupertinoNavigationBar(
         leading: Icon(CupertinoIcons.home),
         middle: Text('Home'),
