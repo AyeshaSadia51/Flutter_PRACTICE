350353e82ad89ad48c855af1cd3e4e842d88bce1
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-10-05 17:32:35 +0600

    AspectRatio, FractionallySizedBox, Flexible, Expanded

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget{
@@ -8,53 +6,103 @@ class HomeScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-
-    // MediaQuery
-    /*
-    Size size = MediaQuery.sizeOf(context);
-    print(size);
-    Orientation orientation = MediaQuery.orientationOf(context);
-    print(orientation);
-    List<DisplayFeature> displayFeatures = MediaQuery.displayFeaturesOf(context);
-    print(displayFeatures);
-    print(MediaQuery.devicePixelRatioOf(context));
-    */
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Responsive'),
       ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          print(constraints.maxWidth);
-          if(constraints.maxWidth > 450) {
-            return Center(child: Text('Too big Screen',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),);
-          } else {
-            return Center(
-              child: OrientationBuilder(builder: (context, orientation){
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(orientation == Orientation.portrait ? 'Portrait Mode' : 'Landscape Mode',
-                      style: TextStyle(fontSize: 28),
-                    ),
-                    Wrap(
-                      alignment: WrapAlignment.center,
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      children: [
-                        Text('When you come from people having zero expectations of'),
-                        Text('who you could be,'),
-                        Text('there’s a fearlessness; you can only go up from there.'),
-                        Text('Don’t fight back that rhetoric with words, but with actions.'),
-                      ],
-                    ),
-                  ],
-                );
-              }),
-            );
-          }
-        }
+      body: Column(
+        children: [
+          // AspectRatio
+          AspectRatio(
+              aspectRatio: 16/4,
+              child: Container(
+                color: Colors.amber,
+                child: Image.network('https://gifdb.com/images/high/cute-cartoon-peach-cat-dance-mmqdbpqu279gtdz0.webp', fit: BoxFit.cover),
+              ),
+          ),
+
+          // FractionallySizedBox
+          Container(
+            //width: MediaQuery.sizeOf(context).width,
+            //height: MediaQuery.sizeOf(context).width,
+            width: MediaQuery.sizeOf(context).width,
+            height: 250,
+            color: Colors.pink,
+            child: FractionallySizedBox(
+              widthFactor: 0.5,
+              heightFactor: 0.7,
+              child: Container(
+                color: Colors.amber,
+                child: Image.network('https://gifdb.com/images/high/cute-cartoon-peach-cat-dance-mmqdbpqu279gtdz0.webp', fit: BoxFit.cover),
+              ),
+            ),
+          ),
+
+          // Flexible
+          Flexible(
+            fit: FlexFit.tight,
+              flex: 2,
+              child: Container(
+                color: Colors.amber,
+                width: MediaQuery.sizeOf(context).width,
+              )
+          ),
+
+          Flexible(
+            fit: FlexFit.tight,
+            flex: 1,
+            child: Row(
+              children: [
+                Flexible(
+                  child: Container(
+                    color: Colors.black,
+                    width: MediaQuery.sizeOf(context).width,
+                  ),
+                ),
+                Flexible(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.blueGrey,
+                    width: MediaQuery.sizeOf(context).width,
+                  ),
+                )
+              ],
+            ),
+          ),
+
+          Flexible(
+              fit: FlexFit.tight,
+              flex: 3,
+              child: Container(
+                color: Colors.green,
+                width: MediaQuery.sizeOf(context).width,
+              )
+          ),
+
+          // Expanded
+          Expanded(
+            flex: 2,
+              child: Container(
+                color: Colors.purple,
+              )
+          ),
+          Expanded(
+            flex: 3,
+              child: Container(
+                color: Colors.deepOrangeAccent,
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: SizedBox(
+                  width:200,
+                  height: 60,
+                  child: FittedBox(
+                    child: Text('Ayesha Ferdous Sadia', style: TextStyle(fontWeight: FontWeight.bold),),
+                  ),
+
+                ),
+              )
+          )
+        ],
       )
     );
   }
