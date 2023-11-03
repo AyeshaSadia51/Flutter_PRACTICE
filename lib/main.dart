826cc5b7664f46599d9a914cf30910bb99b038e9
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-11-03 21:46:14 +0600

    crud api

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/home_screen.dart';
 
 void main() {
-  runApp(const TodoApp());
+  runApp(const CrudApp());
 }
 
-class TodoApp extends StatelessWidget {
-  const TodoApp({super.key});
+class CrudApp extends StatelessWidget {
+  const CrudApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
